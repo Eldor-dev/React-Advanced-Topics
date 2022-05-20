c97732f518e70fab748d414b850bc6bedafdb161
Eldor Azimov <eldorazimov37@gmail.com>
2022-05-20 19:45:11 +0900

    The useEffect Hook.

--- src/hooks/Counter.jsx
+++ src/hooks/Counter.jsx
@@ -1,8 +1,16 @@
-import React, { Fragment, useState } from "react";
+import React, { Fragment, useEffect, useState } from "react";
 
 export default function Counter() {
   const [count, setCount] = useState(0);
   const [name, setName] = useState("");
+
+  useEffect(() => {
+    document.title = `${name} has clicked ${count} times!`;
+
+    return () => {
+      console.log("Clean up");
+    };
+  });
 
   return (
     <Fragment>
